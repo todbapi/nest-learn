28c760ff9bc060f290a299cd50050557c559e9c0
Bapi Dey <todbapi@gmail.com>
2023-09-30 08:37:22 +0530

    extend model. http api hitter.

--- src/cat.model.ts
+++ src/cat.model.ts
@@ -1,16 +1,30 @@
 import { prop } from '@typegoose/typegoose';
 
+class Ts {
+  @prop()
+  year: number;
+
+  @prop({ type: () => [Number] })
+  weekDays: number[];
+}
+
 class Job {
   @prop()
   public title: string;
 
   @prop()
   public position: string;
+
+  @prop({ type: () => [Ts] })
+  ts?: Ts[];
 }
 
 export class Cat {
   @prop({ required: true })
-  name: string;
+  public name: string;
+
+  @prop()
+  type: string;
 
   @prop({ type: () => [Job] })
   public jobs?: Job[];
